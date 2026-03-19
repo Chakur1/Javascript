6d8d62a6eb963f5a00dcc9cba7d5412aa2afadaa
Pedro <ppchakur@gmail.com>
2026-03-19 14:07:55 -0300

    .

--- Aprendizado JS/funcoes.js
+++ Aprendizado JS/funcoes.js
@@ -1,11 +1,26 @@
-function saudacao(nome) {
-    console.log('Olá,', nome)
-}
+//function saudacao(nome) {
+//    console.log('Olá,', nome)
+//}
+
+
+//const saudacao = (nome) => {
+//    console.log('vida longa e prósperam,', nome)
+//}
+
+const saudacao = nome => console.log('Olá,',nome)
+
 
 saudacao('Pedro')
 
-function calcularDobroDeUm(numero){
-    return numero * 2
-}
+//function calcularDobroDeUm(numero){
+//    return numero * 2
+//}
+
+//const calcularDobroDeUm = (numero) => {
+//    return numero * 2
+//}
+
+const calcularDobroDeUm = numero => numero * 2
+
 const numeroDobrado= calcularDobroDeUm(4)
 console.log('o Dobro de 4 é: ' +numeroDobrado)
