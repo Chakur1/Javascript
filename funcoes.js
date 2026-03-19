bc5474fee77741079520927aadd95a9962ac8d70
Pedro <ppchakur@gmail.com>
2026-03-19 13:05:53 -0300

    # Changes to be committed: .

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+function saudacao(nome) {
+    console.log('Olá,', nome)
+}
+
+saudacao('Pedro')
+
+function calcularDobroDeUm(numero){
+    return numero * 2
+}
+const numeroDobrado= calcularDobroDeUm(4)
+console.log('o Dobro de 4 é: ' +numeroDobrado)
